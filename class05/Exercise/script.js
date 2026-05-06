b7c2963d3dec8aa931dfcce511925a7e6076cc11
kamyar <kamyarkarimi98@gmail.com>
2026-05-06 14:17:20 -0400

    Session 05 exercise update

--- class05/Exercise/script.js
+++ class05/Exercise/script.js
@@ -1,3 +1,8 @@
+// Ex 01
+
 // fetch from https://jsonplaceholder.typicode.com/users
 
+// addeventlistener to button to load the users
 // update the DOM with the first 5 users as li elements.
+
+// hint: try fetching just users without any ids.
