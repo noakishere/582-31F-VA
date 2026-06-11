c024ceb89a950f232a801feb079f420c69f33124
kamyar <kamyarkarimi98@gmail.com>
2026-06-11 14:23:57 -0400

    exercises

---
+++ class15/exercises/script.js
@@ -0,0 +1,8 @@
+// 1. create a custom element called:
+//              <movie-card></movie-card>
+
+// this element should accept these attributes:
+
+// 1. title 2. year 3. rating
+
+// render it on your html file.
