f9cea28647b654cca201e13d0492df0898f20640
kamyar <kamyarkarimi98@gmail.com>
2026-06-11 14:25:05 -0400

    exercises

--- class15/exercises/script.js
+++ class15/exercises/script.js
@@ -3,6 +3,33 @@
 
 // this element should accept these attributes:
 
-// 1. title 2. year 3. rating
+//   1. title 2. year 3. rating
 
 // render it on your html file.
+
+// 2. refactor a basic custom element
+
+class HelloBox extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div>
+        <h2>Hello!</h2>
+        <p>Welcome to custom elements.</p>
+      </div>
+    `;
+  }
+}
+
+customElements.define("hello-box", HelloBox);
+
+// refactor it so that connectedCallback only calls render()
+// All HTML generation happens inside render()
+
+// 3. create another GameCard custom element with the following:
+
+//      1. connectedCallback()
+//      2. getTitle()
+//      3. getYear()
+//      4. getRating()
+//      4.b formats Rating  -- ratingFormatter() X/5
+//      5. render
