8c64ac2d210458d08c1a6d5672d3a55ef2284398
kamyar <kamyarkarimi98@gmail.com>
2026-06-17 13:40:35 -0400

    shadow-dom intro

---
+++ class16/script.js
@@ -0,0 +1,102 @@
+// Review
+
+class UserCard extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <div>
+            <h2>Alice</h2>
+            <p>Role: Student</p>
+        </div>
+        `;
+  }
+}
+
+customElements.define("user-card", UserCard);
+
+// ^^ this works, but:
+
+// 1. what if page-wide CSS changes the inside of this component?
+// 2. what if the component's own styles affect other parts of the page?
+// 3. what if we want a reusable element that behaves more like an isolated widget?
+
+// Shadow DOM
+//          creates a separate internal DOM tree for an element.
+// a component can have:
+// - its own internal HTML structure
+// - its own internal styles
+// - better protection from outside styling and markup conflicts
+
+// Light DOM --> regular page DOM outside the component
+//                       EX: document.getElementsByTagName("p")
+
+// Shadow DOM -->  the internal DOM attached to a custom element.
+
+class HelloShadow extends HTMLElement {
+  connectedCallback() {
+    // creating the shadow root for the element.
+    const shadow = this.attachShadow({ mode: "open" });
+    // mode: "open" means the shadow root can still be accessed in JS if needed.
+
+    // We render the component's internal content inside the shadow root and
+    // not inside the normal element body.
+    shadow.innerHTML = `
+    <style>
+        h2 {
+            color: blue;
+        }
+    </style>
+
+    <div>
+        <h2>Hello from Shadow DOM</h2>
+        <p>This content lives inside the shadow root.</p>
+    </div>
+    `;
+  }
+}
+// we're still rendering the element, but its internal structure belongs to
+// its own DOM layer.
+
+customElements.define("hello-shadow", HelloShadow);
+
+// Without Shadow DOM:
+// - outside page styles may change your component unexpectedly
+// - your component's styles may leak into the rest of the page
+
+// With Shadow DOM:
+// - internal styles stay scoped to the component
+// - component structure becomes more self-contained!
+
+/**
+ * Scoped Styles inside Shadow DOM
+ */
+
+class FancyCard extends HTMLElement {
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+
+    // style only affects the HTML inside the shadow root
+    shadow.innerHTML = `
+        <style>
+         .card {
+            border: 2px solid purple;
+            padding: 1rem;
+            background: #f1e3ff;
+         }
+
+         h2 {
+            color: purple;
+            margin-top: 0;
+         }
+
+        </style>
+
+
+        <div class="card">
+            <h2>Shadow Card</h2>
+            <p>This style is scoped inside the component.</p>
+        </div>
+        
+        `;
+  }
+}
+customElements.define("fancy-card", FancyCard);
