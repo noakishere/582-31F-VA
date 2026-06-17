e5db0121fa2787a9fdc540861dd4024a1ec74f03
kamyar <kamyarkarimi98@gmail.com>
2026-06-17 14:22:42 -0400

    exercise

---
+++ class16/exercise/script.js
@@ -0,0 +1,24 @@
+/**
+ * Create a custom element called:
+ *  <movie-box></movie-box>
+ *
+ * Requirements:
+ *
+ * The element must:
+ *
+ *  use Shadow DOM
+ *  accept attributes:
+ *      title
+ *      year
+ *      director
+ *      poster
+ *
+ * render a styled card inside the shadow root
+ *
+ * use helper methods
+ *
+ * example HTML:
+ * <movie-box title="MOVIE_TITEL" year="0000" director="DIRECTOR_NAME"
+ *  poster-url="POSTER_URL"></movie-box>
+ *
+ */
