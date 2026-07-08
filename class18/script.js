e49a8092d092270d3be43421ffa87088269859eb
kamyar <kamyarkarimi98@gmail.com>
2026-07-08 13:19:42 -0400

    template init

---
+++ class18/script.js
@@ -0,0 +1,19 @@
+/**
+ * Templates
+ * storing reusable HTML structure and cloning when needed
+ */
+
+// We build UI with:
+//  innerHTML
+//  createElement()
+//  modules
+//  Custom Elements
+//  Shadow DOM
+
+// What is a template?
+
+// A <template> is a chunk of HTML stored in the page, but not rendered immediately.
+
+//      it exists in the document
+//      but it does not show on the page BY DEFAULT.
+//          it is up to JS to clone and insert it afterwards.
